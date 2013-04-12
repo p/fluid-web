0b1c85b1b0e0231422f60f6a047cc2ffb056a8e2
Oleg Pudeyev <oleg@bsdpower.com>
2013-04-12 19:53:23 -0400

    Handle js in addition to css

--- build.py
+++ build.py
@@ -13,11 +13,12 @@ var styles = {};
 var matched = false;
 '''
 
-hostmap = {}
+css_hostmap = {}
+js_hostmap = {}
 
-for file in os.listdir('sites'):
-    if not file.endswith('css'):
-        continue
+def process_css(file):
+    global css_hostmap
+    
     with open(os.path.join('sites', file)) as f:
         content = f.read()
     match = re.compile('^host: (.*)', re.M).search(content)
@@ -27,12 +28,34 @@ for file in os.listdir('sites'):
     content = re.compile(r'/\*.*?\*/', re.S).sub('', content)
     content = content.replace("\n", ' ').replace('"', r'\"')
     
-    bits = hostmap.get(host, [])
+    bits = css_hostmap.get(host, [])
     bits.append(content)
-    hostmap[host] = bits
+    css_hostmap[host] = bits
 
-for host in hostmap:
-    content = ''.join(hostmap[host])
+def process_js(file):
+    global js_hostmap
+    
+    with open(os.path.join('sites', file)) as f:
+        content = f.read()
+    match = re.compile('^host: (.*)', re.M).search(content)
+    assert match
+    host = match.group(1)
+    
+    content = re.compile(r'/\*.*?\*/', re.S).sub('', content)
+    content = content.replace("\n", ' ').replace('"', r'\"')
+    
+    bits = js_hostmap.get(host, [])
+    bits.append(content)
+    js_hostmap[host] = bits
+
+for file in os.listdir('sites'):
+    if file.endswith('.css'):
+        process_css(file)
+    if file.endswith('.js'):
+        process_js(file)
+
+for host in css_hostmap:
+    content = ''.join(css_hostmap[host])
     preamble += '''
         styles['%(host)s'] = "%(content)s";
         if (/\/\/%(host)s/.test(window.location.href)) {
@@ -41,8 +64,17 @@ for host in hostmap:
         }
     ''' % dict(host=host, content=content)
 
-if 'default' in hostmap:
-    content = ''.join(hostmap['default'])
+for host in js_hostmap:
+    content = ''.join(js_hostmap[host])
+    preamble += '''
+        if (/\/\/%(host)s/.test(window.location.href)) {
+            %(content)s
+            ;matched = true;
+        }
+    ''' % dict(host=host, content=content)
+
+if 'default' in css_hostmap:
+    content = ''.join(css_hostmap['default'])
     preamble += '''
         styles['default'] = "%(content)s";
         if (!matched) {

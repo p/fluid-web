12dd7b53abbd9f030ed46ffc57749f6d2fda3a97
Oleg Pudeyev <oleg@bsdpower.com>
2013-07-30 17:08:35 -0400

    Finally got redirect from google to google proxy working

--- sites/google.js
+++ sites/google.js
@@ -5,7 +5,33 @@ host: www.google.com
 //alert(1);
 
 if (window.location == window.top.location) {
-  window.location = window.location.href.replace('www.google.com', 'gs');
-  alert(window.location.href.replace('www.google.com', 'gs'));
-  //alert(window.location);
+  // window.location does not seem to work - overridden by google js?
+  // googles grabby hands seem to catch nearly everything
+  //var head = document.getElementsByTagName('head')[0];
+  var url = window.location.href.replace('www.google.com', 'gs');
+  //var meta = document.createElement('meta');
+  //meta.att
+  //head.appendChild(meta);
+  //alert(url);
+  //head.innerHTML += '<meta http-equiv="refresh" content="0;url=' + url + '">';
+  window.bsdpower.ready(function() {
+  var body = document.getElementsByTagName('body')[0];
+  body.innerHTML += '<form id=rr method=get action="http://gs/x"><input type=submit></form>';
+    //setTimeout(function() {
+    var form = document.getElementById('rr');
+    form.action = url.split('?')[0];
+    var pairs = location.href.split('?')[1].split('&');
+    for (var i = 0; i < pairs.length; ++i) {
+      var kw = pairs[i].split('=');
+      var child = document.createElement('input');
+      child.type = 'hidden';
+      child.name = kw[0];
+      child.value = kw[1];
+      form.appendChild(child);
+    }
+    //alert(form.innerHTML);
+      form[0].click();
+    //}, 100);
+    //alert('ok');
+  });
 }

cfbc39fcf57c83ab73a1b646e8dbf67581a98de9
Oleg Pudeyev <oleg@bsdpower.com>
2013-07-30 08:54:00 -0400

    More sites

---
+++ sites/github-extra.js
@@ -0,0 +1,12 @@
+/**
+host: github.com
+*/
+
+window.bsdpower.ready.push(function($) {
+  $('.numbers-summary').each(function() {
+    var starturl = this.childNodes[this.childNodes.length-1].childNodes[0].href;
+    alert(starturl);
+    var li = $('<li/>');
+    //li[0].appendChild
+  });
+});

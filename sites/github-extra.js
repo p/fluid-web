50f7d49be4fe5149b470b2356668fb7c97a25457
Oleg Pudeyev <oleg@bsdpower.com>
2013-08-09 17:31:14 -0400

    Pull requests link addon for github

--- sites/github-extra.js
+++ sites/github-extra.js
@@ -2,11 +2,12 @@
 host: github.com
 */
 
-window.bsdpower.ready(function($) {
+window.bsdpower.ready(function() {
   $('.numbers-summary').each(function() {
-    var starturl = this.childNodes[this.childNodes.length-1].childNodes[0].href;
-    alert(starturl);
-    var li = $('<li/>');
-    //li[0].appendChild
+    var url = $('.numbers-summary li:first-child a').attr('href');
+    var pullsurl = url.replace(/\/commits\/.*/, '/pulls');
+    var li = $('<li><a href="">Pull requests</a></li>');
+    li.find('a').attr('href', pullsurl);
+    li.insertAfter($('.numbers-summary li:first-child'));
   });
 });
